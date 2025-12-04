0fadbcdadc23c6882ea7693eb76152ba28773779
Otabek <sadiridinovotabek@gmail.com>
2025-12-04 10:00:28 +0500

    refactor: answer

--- muhammadaziz.js
+++ muhammadaziz.js
@@ -2,5 +2,7 @@ console.log('Pycharm is the best IDE for programming.')
 
 console.log("Papka o'zbek tilida nima bo'ladi?")
 console.log('Botirov Muhammadali: Bilmadim!')
+console.log('No, it will be: "Yorliq"');
+
 
 // JILD
